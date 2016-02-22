62472ec80fd0718891d6b2c5f087c8cd8e3cc371
kpa25 <kahini.amin1@gmail.com>
2016-02-21 23:38:52 -0500

    Final Comment changes

--- CacheSolve.R
+++ CacheSolve.R
@@ -1,12 +1,15 @@
 cacheSolve <- function(x, ...) {
 
+  ## will return the inverse of the orignally inputed matrix in MakeCacheMatrix function
   inv = x$getinv()
   
+  ## in the case that the inverse is already calcualted
   if (!is.null(inv)){
     message("Recieving Cache Data")
     return(inv)
   }
   
+  ## or else, calulates the inverse
   mat.data = x$get()
   inv = solve(mat.data, ...)
   
--- MakeCacheMatrix.R
+++ MakeCacheMatrix.R
@@ -1,7 +1,10 @@
 MakeCacheMatrix <- function(x = matrix()) {
 
-  
+  ## the following funtion will return a list containing the following functions:
+  ##  1. Set the matrix, 2. Get he matrix, 3. set inverse, 4. get the inverse, later to be used as the input for cacheSolve function
   inv = NULL
+  
+  ## <<- is used to give a value to an object which resides outside the current enviornment 
   set = function(y) {
     x <<- y
     inv <<- NULL
